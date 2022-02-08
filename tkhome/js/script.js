a60e9d19aa76f1a2f9167f8cb629ce934f9eef7b
atsi87 <atsimam@gmail.com>
2022-02-07 22:50:48 -0800

    new take home file

---
+++ tkhome/js/script.js
@@ -0,0 +1,13 @@
+
+
+// single comment 
+
+// <!--  multi line comment
+
+console.log('Atsedemariam kassie');
+
+myFavorite = (['shiro', 'pizza', 'doro', 'mesir', 'enjera']);
+console.log(myFavorite);
+
+// document.body.getElementById('food').innerHTML = 'food';
+onclick = "document.getElementById('food').innerHTML = food()";
